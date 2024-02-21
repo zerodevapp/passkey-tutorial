e81953544d1834c734169edf379e66d1622b4d3e
Derek Chiang <me@derekchiang.com>
2024-02-21 00:15:05 -0500

    Update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ZeroDev Passkeys + Session Keys Demo",
+  title: "ZeroDev Passkeys Demo",
   description: "",
 };
 
